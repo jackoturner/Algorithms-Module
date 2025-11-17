f7dcde6276fc5571b94d2b2511ccdf8a890c6f6c
Jack Turner <jackoturner06@icloud.com>
2025-11-17 09:37:02 +0000

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
-#include <stack>
 #include <string>
+#include <cctype>
+#include <sstream>
+
+using namespace std;
+
+struct Node {
+    int position;
+    char letter; // lowercase
+    char part;   // 'f' first, 'm' middle, 'l' last
+    Node* next;
+    Node(int pos, char c, char p) : position(pos), letter(c), part(p), next(nullptr) {}
+};
+
+class Full_Name {
+private:
+    Node* head;
+    int size;
+
+public:
+    
+
+}
